aa1330d214e48527d93827a596b30eb91b0c933d
Edimarpcosta <edimarpcosta@hotmail.com>
2025-05-17 01:55:27 -0300

    Update script.js

--- ameripan/rotas/bruno/equipes/panificacao/script.js
+++ ameripan/rotas/bruno/equipes/panificacao/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     // ========= CONFIGURAÇÃO DA APLICAÇÃO =========
     const AppConfig = {
-        spreadsheetId: "1vWWvnYGgZyqVYMoBfcciCCGgEN1FLzSrmXYPBOPKUjk",
+        spreadsheetId: "1f03fl_8HI5V6moTLYNMsQLFoi3EsLYZQI23CKkHqsow",
         apiKey: "AIzaSyChiZPUY-G3oyZN2NGY_vlgRXUzry9Pkeo", // Mantenha sua chave aqui
-        range: "novas-rotas",
+        range: "panificacao",
         distribuidoraAmeripanCoords: L.latLng(-22.730986246840104, -47.358144521713264),
         geoJsonSources: [
             {
